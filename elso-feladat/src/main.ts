97e9d6cb3c7a6743dd993216c308f4d4bff8551b
Petrik Diak <diak@petrik.hu>
2026-09-02 12:26:59 +0200

    eddig

--- elso-feladat/src/main.ts
+++ elso-feladat/src/main.ts
@@ -1,60 +1,44 @@
-import './style.css'
-import heroImg from './assets/hero.png'
-import typescriptLogo from './assets/typescript.svg'
-import viteLogo from './assets/vite.svg'
-import { setupCounter } from './counter.ts'
+interface Adat {
+  szoveg : string;
+  datum : Date;
+  szam : number;
+  munka : string;
+}
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${typescriptLogo}" class="framework" alt="TypeScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.ts</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
 
-<div class="ticks"></div>
+let adatok : Adat[] = [];
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://www.typescriptlang.org" target="_blank">
-          <img class="button-icon" src="${typescriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+async function AdatokBetoltese() {
+  const response = await fetch("https://retoolapi.dev/3U6bLA/data")
+  adatok = await response.json();
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+  console.log(adatok);
+  talbaMegjelenitese();
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+
+function talbaMegjelenitese() {
+    const tabla = document.getElementById("tabla");
+
+
+    tabla.innerHTML = "";
+
+    adatok.forEach((adat) => {
+        tabla.innerHTML += `
+            <tr>
+                <td>${adat.szoveg}</td>
+                <td>${adat.datum}</td>
+                <td>${adat.szam}</td>
+                <td>${adat.munka}</td>
+                <td>
+                    <button class="btn btn-danger">
+                        Törlés
+                    </button>
+                </td>
+            </tr>
+        `;
+    });
+
+}
+
+AdatokBetoltese();
